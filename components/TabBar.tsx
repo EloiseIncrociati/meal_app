c22463efb5d0e17887eb4a86c32e64f5929fb9df
EloiseIncrociati <60602575+EloiseIncrociati@users.noreply.github.com>
2025-02-01 12:44:44 +0100

    initial commit + bottom tab navigator

---
+++ components/TabBar.tsx
@@ -0,0 +1,89 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { useLinkBuilder, useTheme } from "@react-navigation/native";
+import { PlatformPressable } from "@react-navigation/elements";
+
+const TabBar = ({ state, descriptors, navigation }) => {
+  const primaryColor = "#a44cff"; //edit here to change color text navbar
+  const inactiveColor = "#4a5568";
+  const { buildHref } = useLinkBuilder();
+
+  return (
+    <View style={styles.tabbar}>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key];
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name;
+
+        if (["_sitemap", "+not-found"].includes(route.name)) return null;
+
+        const isFocused = state.index === index;
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: "tabPress",
+            target: route.key,
+            canPreventDefault: true,
+          });
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name, route.params);
+          }
+        };
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: "tabLongPress",
+            target: route.key,
+          });
+        };
+
+        return (
+          <PlatformPressable
+            key={route.name}
+            style={styles.tabbarItem}
+            href={buildHref(route.name, route.params)}
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarButtonTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}>
+            <Text style={{ color: isFocused ? primaryColor : inactiveColor }}>
+              {label}
+            </Text>
+          </PlatformPressable>
+        );
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  tabbar: {
+    position: "absolute",
+    bottom: 25,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "white",
+    marginHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 25,
+    borderCurve: "continuous",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 10 },
+    shadowRadius: 10,
+    shadowOpacity: 0.1,
+  },
+  tabbarItem: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+
+export default TabBar;
